7e9dac15c19e8371ddb3f2415eb474461eb36128
Jaron Morris <jaron.c.morris@gmail.com>
2022-06-14 18:25:44 -0800

    modal placeholder

--- src/app/job-list/job-list.component.ts
+++ src/app/job-list/job-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JobPost } from '../models/job';
 import { JobsService } from '../services/jobs.service';
+declare var jQuery: any;
 
 
 @Component({
@@ -15,10 +16,48 @@ export class JobListComponent implements OnInit {
   constructor(private jobsService: JobsService) { }
 
   ngOnInit(): void {
-    this.jobsService.getJobList().subscribe(results =>
-      {this.JobPosts = results});
+    this.jobsService.getJobList().subscribe(results => { this.JobPosts = results });
+
+    // Show Modal on click
+    var modal = document.getElementById('jobModal');
+    var closeBtn = document.getElementById('closeBtn');
+    var modalBtn = document.getElementById('openModalBtn')
+
+    //Listen for click
+    modalBtn.addEventListener('click', openModal);
+
+    //Open Modal function
+    function openModal() {
+      modal.style.display = 'block';
+    }
+
+    //listen for close click
+    closeBtn.addEventListener('click', closeModal);
+    window.addEventListener('click', clickOutside);
+
+    //Close Modal function
+    function closeModal() {
+      modal.style.display = 'none';
+    }
+    function clickOutside(e) {
+      if (e.target == modal){
+        modal.style.display = 'none';
+      }
+    }
   }
 
-  
+
 
 }
+
+// // Show Modal on click
+// var modal = document.getElementById('jobModal');
+// var closeBtn = document.getElementById('closeBtn');
+// var modalBtn = document.getElementById('openModalBtn')
+
+// //Listen for click
+// modalBtn.addEventListener('click', openModal);
+
+// function openModal(){
+//   console.log('123');
+// }
